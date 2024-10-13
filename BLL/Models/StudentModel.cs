3c0c3941d8c4ec772789be67e62625791d7e8a9a
cakmakenes <enes.cakmak@ug.bilkent.edu.tr>
2024-10-13 13:19:29 +0300

    Add project files.

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿#nullable disable //I added this line to disable nullable warnings but DateTime and decimal are still nullable,
+                  // so I had to put ? to make them nullable, Why this line is not working?
+using BLL.DAL;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record { get; set; }
+
+        public string Name => Record.Name;
+
+        public string Surname => Record.Surname;
+
+        public DateTime? Birthdate => Record.BirthDate;
+
+        public decimal? Gpa => Record.Gpa;
+    }
+}
