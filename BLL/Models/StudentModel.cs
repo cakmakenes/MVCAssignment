160ffba056f60a1ac66f2351e52d779f3caf1177
cakmakenes <enes.cakmak@ug.bilkent.edu.tr>
2024-10-21 20:35:19 +0300

    Scaffolds completed. Second assignment finished.

--- BLL/Models/StudentModel.cs
+++ BLL/Models/StudentModel.cs
@@ -1,4 +1,4 @@
-﻿#nullable disable //I added this line to disable nullable warnings but DateTime and decimal are still nullable,
+﻿#nullable enable //I added this line to disable nullable warnings but DateTime and decimal are still nullable,
                   // so I had to put ? to make them nullable, Why this line is not working?
 using BLL.DAL;
 
@@ -12,7 +12,7 @@ namespace BLL.Models
 
         public string Surname => Record.Surname;
 
-        public DateTime? Birthdate => Record.BirthDate;
+        public DateTime? BirthDate => Record.BirthDate;
 
         public decimal? Gpa => Record.Gpa;
     }
